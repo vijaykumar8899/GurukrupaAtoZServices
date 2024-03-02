7c70b074cc3030e54d69c5756b2c5324adce6a9a
vijaykumar8899 <76232939+vijaykumar8899@users.noreply.github.com>
2024-03-02 20:56:36 +0530

    Add files via upload
    coded the services part and made everything responsive

--- src/components/container.jsx
+++ src/components/container.jsx
@@ -1,13 +1,13 @@
 import "../Styles/main.css";
 import "../Styles/container.css";
 
-function Container() {
+function Container({ title = "" }) {
   return (
     <div className="container">
       <img id="containerImage" src="../assets/img1.png" alt="Your Image" />
       <br />
       <div className="text-container">
-        <p>Insurance</p>
+        <p>{title}</p>
         <p>Consultancy</p>
         <br />
         <button id="pdfButton">Get Brochure</button>
--- src/components/our-services.jsx
+++ src/components/our-services.jsx
@@ -4,10 +4,17 @@ function OurServices() {
   return (
     <div>
       <h2 className="our-services-heading">Our Services</h2>
-      <div className="container-row">
-        <Container />
-        <Container />
-       
+      <div className="our-services-div">
+        <div className="container-row">
+          <Container title="one" />
+          <Container title="two" />
+          <Container title="three" />
+        </div>
+        <div className="container-row">
+          <Container title="four" />
+          <Container title="five" />
+          <Container title="six" />
+        </div>
       </div>
     </div>
   );

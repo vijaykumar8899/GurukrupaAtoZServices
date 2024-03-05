91d3e17d6a65c4aa8c17ee45b11ece15fa544313
vijaykumar8899 <76232939+vijaykumar8899@users.noreply.github.com>
2024-03-05 17:06:16 +0530

    Add files via upload
    made the arrow-and-text responsive and also the about-us section responsive for smaller screens.

--- src/components/about-us.jsx
+++ src/components/about-us.jsx
@@ -6,9 +6,8 @@ const AboutUs = () => {
     <div id="home">
       <h2 className="our-services-heading">About Us</h2>
       <p className="about-paragraph">
-        Welcome to{" "}
-        <strong className="highlighted-text">Gurukrupa AtoZ Services</strong>,
-        your ultimate destination for a wide range of essential services. Our
+        Welcome to <strong className="highlighted-text">Gurukrupa AtoZ Services</strong>,your
+        ultimate destination for a wide range of essential services. Our
         dedicated team of professionals is here to guide you through insurance,
         job placement, real estate transactions, and digital shopping, ensuring
         you have expert support every step of the way. Beyond the basics, we

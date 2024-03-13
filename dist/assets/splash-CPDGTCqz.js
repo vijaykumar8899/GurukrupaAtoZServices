a91f13bab4e64b57b8360d11352a8667b0169af4
vijaykumar8899 <jaykumarm416@gmail.com>
2024-03-13 22:20:33 +0530

    changed clr of nav for smaller screens

---
+++ dist/assets/splash-CPDGTCqz.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-DH_u5Aen.js";const e=()=>s.jsx("div",{className:"shape",children:s.jsx("div",{className:"splash",children:"this is container"})});export{e as default};
---
+++ dist/assets/title-DI3fPJVw.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-DH_u5Aen.js";const r=()=>t.jsx("div",{children:t.jsx("h1",{id:"title",className:"title",children:"GurukrupaAtoZServices"})});export{r as default};
